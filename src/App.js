606e5c0d83c171967f16a5640c9fe3ab3dad3706
ansarsufiyana <ansarali106@gmail.com>
2022-08-03 05:53:26 +0500

    added react router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import './App.css';
-import Classcomponent from './components/class/Classcomponent';
-import User from './components/functional/User';
-import Input from './components/input/Input';
-import Props from './components/props/Props';
-import Variable from './components/variable/Variable';
-
-
+import { Route, Routes,Link } from "react-router-dom";
+import "./App.css";
+import Classcomponent from "./components/class/Classcomponent";
+import User from "./components/functional/User";
+import Input from "./components/input/Input";
+import Props from "./components/props/Props";
+import Variable from "./components/variable/Variable";
 
 function App() {
   return (
     <div>
-      <Classcomponent />
-      <User />
-      <Variable />
-      <Props />
-      <Input />
+    <nav>
+      <ul>
+        <li><Link to="/">User</Link></li>
+        <li><Link to="/class">Classcomponent</Link></li>
+        <li><Link to="/variable">Variable</Link></li>
+        <li><Link to="/props">Props</Link></li>
+        <li><Link to="/input">Input</Link></li>
+      </ul>
+    </nav>
+      <Routes>
+        <Route path="/" element={<User />} />
+        <Route path="/class" element={<Classcomponent />} />
+        <Route path="/variable" element={<Variable />} />
+        <Route path="/props" element={<Props />} />
+        <Route path="/input" element={<Input />} />
+      </Routes>
     </div>
   );
 }
--- src/components/input/Input.jsx
+++ src/components/input/Input.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 
 function Input() {
+  function getData (val){
+    console.warn(val)
+  }
   return (
     <div>
         <div> 
             <h1>Get the value !</h1>
-            <input style={{margin:20}} type="text" onChange={} />
+            <input style={{margin:20}} type="text" onChange={getData} />
         </div>
     </div>
   )
